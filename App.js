4fe5c363158155cce79ecc70bdb09e0672670451
Rudá Torquato <rudatorquato@gmail.com>
2021-05-06 22:52:27 -0300

    Componente Botão #01

--- App.js
+++ App.js
@@ -1,22 +1,39 @@
 import React from 'react'
 import {  StyleSheet, Text, View } from 'react-native'
+import Button from './src/components/Button'
 
 export default () => (
     <View style={styles.container}>
-      <Text style={styles.welcome}>FOI</Text>
+     <View style={styles.buttons}>
+       <Button label='AC' />
+       <Button label='/' />
+       <Button label='7' />
+       <Button label='8' />
+       <Button label='9' />
+       <Button label='*' />
+       <Button label='4' />
+       <Button label='5' />
+       <Button label='6' />
+       <Button label='-' />
+       <Button label='1' />
+       <Button label='2' />
+       <Button label='3' />
+       <Button label='+' />
+       <Button label='0' />
+       <Button label='.' />
+       <Button label='=' />
+
+     </View>
     </View>
 )
 
 const styles = StyleSheet.create({
    container: {
     flex:1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor:'#F5FCFF'
    },
-   welcome:{
-     fontSize: 20,
-     textAlign: 'center',
-     margin:10,
+   buttons: {
+     flexDirection: 'row',
+     flexWrap: 'wrap'
    }
+
 })
